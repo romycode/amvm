c9011c28f3f548d9d2ee40dcd3206459fff67117
Albert Romera Mulero <albert.romera.arm@gmail.com>
2022-01-17 00:34:59 +0100

    feat: add fetch command to main

--- cmd/mvm/main.go
+++ cmd/mvm/main.go
@@ -5,19 +5,29 @@ import (
 	"os"
 
 	"github.com/romycode/mvm/internal/app/cmd"
+	"github.com/romycode/mvm/internal/app/config"
+	"github.com/romycode/mvm/pkg/color"
 )
 
 type Command string
 
 const (
-	Info Command = "info"
+	Info  Command = "info"
+	Fetch Command = "fetch"
 )
 
 func main() {
+	conf, err := config.LoadConfiguration()
+	if err != nil {
+		PrintOutput(cmd.NewOutput(color.Colorize(err.Error(), color.Red), 1))
+	}
+
 	command := Command(os.Args[1])
 	switch command {
 	case Info:
 		PrintOutput(cmd.NewInfoCommand().Run())
+	case Fetch:
+		PrintOutput(cmd.NewFetchCommand(*conf).Run())
 	}
 }
 
